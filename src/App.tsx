421caab00f070de50ff08d625fb107778719ce3e
elencio <elenciocalado@gmail.com>
2023-04-19 22:07:33 -0300

    Add Globalstyle and component Header,  Summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { Transactions } from "./pages/Transactions";
 
 export function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+   <ThemeProvider theme={defaultTheme}>
+
+    <GlobalStyle />
+    <Transactions />
+   </ThemeProvider>
   )
 }
 
---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,9 @@
+import { Header } from "../../Components/Header";
+
+export function Transactions(){
+  return (
+    <div>
+     <Header/>
+    </div>
+  )
+}
