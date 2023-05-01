d79ba9a6c6dc1ea71ec82a97a439fdfee233ca9b
elencio <elenciocalado@gmail.com>
2023-04-30 21:46:01 -0300

    Adicionando o componente Sumary no app

--- src/Components/Summary/inde.tsx
+++ src/Components/Summary/inde.tsx
@@ -1,5 +1,32 @@
+import { SummaryContainer, SummaryCard } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+
 export function Summary(){
   return (
-    <h1>Summary</h1>
+    <SummaryContainer>
+        <SummaryCard>
+          <header>
+              <span>Entradas</span>
+              <ArrowCircleUp size={32} color='#00b37e' />
+          </header>
+
+          <strong>R$ 17.400,00</strong>
+        </SummaryCard>
+        <SummaryCard>
+          <header>
+              <span>Saidas</span>
+              <ArrowCircleDown size={32} color='#f75a68' />
+          </header>
+
+          <strong>R$ 17.400,00</strong>
+        </SummaryCard>
+        <SummaryCard>
+          <header>
+              <span>Total</span>
+              <CurrencyDollar size={32} color='#fff' />
+          </header>
+          <strong>R$ 17.400,00</strong>
+        </SummaryCard>
+    </SummaryContainer>
   )
 }
---
+++ src/Components/Summary/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const SummaryContainer = styled.div`
+     width: 100%;
+     max-width: 1120px;
+     margin: 0 auto;
+     padding: 0 1.5rem;
+
+     display: grid;
+     grid-template-columns: repeat(3, 1fr);
+     gap: 2rem;
+
+     margin-top: -5rem;
+     
+
+`
+
+export const SummaryCard = styled.div`
+  
+
+`
